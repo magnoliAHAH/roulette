5dc72c89210c09bfc55430ac3380d78e0d8b8492
magnoliAHAH <mixail.ermin33@gmail.com>
2025-04-04 09:26:08 +0300

    test tgs from api fix

--- frontend/roulette-front/src/App.jsx
+++ frontend/roulette-front/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import Lottie from 'lottie-react';
 import { IoAddCircleOutline } from "react-icons/io5";
 
 function App() {
